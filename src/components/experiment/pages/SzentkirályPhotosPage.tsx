559bc700854b4dad9626635d805bee1646555c9b
phrubos <peter.hrubos.szte@gmail.com>
2026-01-28 16:53:54 +0100

    Fix TypeScript error in SzentkirályPhotosPage by removing unnecessary @ts-expect-error directive

--- src/components/experiment/pages/SzentkirályPhotosPage.tsx
+++ src/components/experiment/pages/SzentkirályPhotosPage.tsx
@@ -32,7 +32,6 @@ export default function SzentkirályPhotosPage() {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.6, delay: 0.2 }}
             >
-                {/* @ts-expect-error - photos is newly added in data.ts */}
                 <PhotoViewer items={data.photos || []} />
             </motion.div>
         </div>
